3e3ffacf6f354a4ba813747753023570496e8953
nob <yanbe.n@jointanet.com>
2015-11-05 15:46:06 +0900

    card positioning tweak.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -5,14 +5,22 @@
       $container.packery({
         itemSelector: '.pitem',
         gutter: -40,
-        percentPosition: true,
         "isOriginTop": false
       });
     });
 
     if (Modernizr.preserve3d) {
       //CSS3 3D effect can be used. initialize Adaptive Modal and use it!
-      $('a.pitem').adaptiveModal({});
+      $('a.pitem').adaptiveModal({
+        beforeAnimate: function(el, status) {
+          //avoid menu-title is loose it's style and been shown in bottom of the flipping card.
+          if(status == 'open') el.children('.menu-title').css('display', 'none');
+        },
+        afterAnimate: function(el, status) {
+          //put the menu-title back to original state after animation.
+          if (status == 'open') el.children('.menu-title').css('display', 'block');
+        }
+      });
     } else {
       // IE(even in IE11) can't handle CSS3 3D Transforms correctly. Use TwtBootstrap Modal.
       $('a.pitem').click(function() {
